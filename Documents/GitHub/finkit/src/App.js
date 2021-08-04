65e603d10269043eeab968738b909487042fd7e6
turtlemooncake <vickitori109@gmail.com>
2021-08-04 17:57:13 -0500

    filtering completed for (2) modals

--- Documents/GitHub/finkit/src/App.js
+++ Documents/GitHub/finkit/src/App.js
@@ -24,6 +24,9 @@ function MyVerticallyCenteredModal(props) {
           Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
           dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
           consectetur ac, vestibulum at eros.
+          sdfsdlfkjsdlkfj 
+          asdfsdf 
+          asdfsdf dfdsfsdfsdfsdfsdfdsfsdfsdfsdfsd
         </p>
       </Modal.Body>
       <Modal.Footer>
@@ -36,27 +39,42 @@ function MyVerticallyCenteredModal(props) {
 function App() {
   const [modalShow, setModalShow] = React.useState(false);
   const [modal2Show, setModal2Show] = React.useState(false);
+  const [hook, setHook] = React.useState('');
+  const FilterHelper = (event) =>{ // obtains input value; bounces back and stores in hook
+    setHook(event.target.value)
+  }
+
+  let defaultMsg = "What are your intentions?"
+
+  // filtering, recommends user which modal to click into
+  if (hook.includes("insurance") || hook.includes("life")){
+    defaultMsg = "->Student Health Insurance";
+  } else if (hook.includes("earn")){
+    defaultMsg = "->Investment Calculator";
+  } else {
+    defaultMsg = "What are your intentions?";
+  }
 
   return (
     <>
     <div>
       <h2 class="title1">F I N K I T</h2>
-      <input class="title"></input>
-      <h2 class="title2">what  are  your  intentions?</h2>
+      <input onChange={FilterHelper} class="title"></input>
+      <h2 class="title2">{defaultMsg}</h2>
     </div>
     <div>
     <button variant="primary" id="investment" class="box1" onClick={() => setModalShow(true)}>
-      Investment
+      Investment Calculator
     </button>
     </div>
     <div>
     <button variant="primary" id = "info" class="box2" onClick={() => setModalShow(true)}>
-      Budgeting 
+      Budgeting Calculator
     </button>
     </div>
     <div>
     <button variant="primary" id = "info" class="box3" onClick={() => setModalShow(true)}>
-      Time Value
+      Time Value Calculator
     </button>
     </div>
     <div>
