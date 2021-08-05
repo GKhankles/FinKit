117886946373520877e5c10996a754c20f1ff048
turtlemooncake <vickitori109@gmail.com>
2021-08-05 01:01:53 -0500

    functionality for 2 tools done

--- Documents/GitHub/finkit/src/App.js
+++ Documents/GitHub/finkit/src/App.js
@@ -5,7 +5,62 @@ import Modal from 'react-bootstrap/Modal';
 import React, { useEffect, useState } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-function MyVerticallyCenteredModal(props) {
+function SavingsModal(props) {
+  const SaveCalculator = () => {
+    const [ival, setIval] = React.useState(0);
+    const [rate, setRate] = React.useState(0);
+    const [years, setYears] = React.useState(0);
+    const [comp, setComp] = React.useState(0);
+    const [total, setTotal] = React.useState(0);
+
+    const IntialValSetter = (event) =>{ // obtains input value; bounces back and stores in hook
+      setIval(event.target.value);
+    }
+
+    const RateSetter = (event) =>{ // obtains input value; bounces back and stores in hook
+      setRate(event.target.value);
+    }
+
+    const YearSetter = (event) =>{ // obtains input value; bounces back and stores in hook
+      setYears(event.target.value);
+    }
+
+    const CompSetter = (event) =>{ // obtains input value; bounces back and stores in hook
+      setComp(event.target.value);
+    }
+
+    const calculate = () =>{
+      let valInt = parseInt(ival, 10);
+      let rateInt = parseFloat(rate);
+      let yrInt = parseInt(years, 10);
+      let cInt = parseInt(comp, 10);
+
+      let stepOne = 1 + (rateInt / cInt); 
+      let expmult = cInt * yrInt; 
+      let stepTwo = stepOne ** expmult;
+      let final = valInt * stepTwo; 
+      final = final.toFixed(2);
+      console.log(valInt, rateInt, yrInt, cInt)
+      setTotal(final);
+    }
+
+    return(
+      <div>
+        <input class="inputPadding" onChange={IntialValSetter}/> <label> initial amt</label>
+        <div></div>
+        <input class="inputPadding" onChange={RateSetter}/> <label> rate (decimal)</label>
+        <div></div>
+        <input class="inputPadding" onChange={YearSetter}/> <label> # years</label>
+        <div></div>
+        <input class="inputPadding" onChange={CompSetter}/> <label> # compound times</label>
+        <div></div>
+        <Button variant="outline-primary" onClick={calculate}>Calculate!</Button>
+        <div></div>
+        <span>{total}</span>
+      </div>
+    );
+  }
+
   return (
     <Modal
       {...props}
@@ -15,18 +70,49 @@ function MyVerticallyCenteredModal(props) {
     >
       <Modal.Header closeButton>
         <Modal.Title id="contained-modal-title-vcenter">
-          Modal heading
+          Savings Compound Interest Calculator
         </Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        <h4>Centered Modal</h4>
+        <SaveCalculator/>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button onClick={props.onHide}>Close</Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
+
+function InfoModal(props) {
+  return (
+    <Modal
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          Student Health Insurance 101
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <h4>Do Young People Need Health Insurance?</h4>
         <p>
-          Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-          dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
-          consectetur ac, vestibulum at eros.
-          sdfsdlfkjsdlkfj 
-          asdfsdf 
-          asdfsdf dfdsfsdfsdfsdfsdfdsfsdfsdfsdfsd
+          Nope. Surprises happen all the team. Health Insurance not only has your back during large accidents, 
+          but also smaller scale illnesses such as strep throat medication. 
+        </p>
+        <h4>What if I am a) and Independent, and b) financially unstable?</h4>
+        <p>
+          No Problem. If your annual income is around $17,300 or under, under the Affordable Care Act you can qualify
+          for Medicaid. 
+        </p>
+        <h4>As a College Student, what is my best bet for cheap health insurance?</h4>
+        <p>
+          Young adults under the age of 26 can still be listed on their parent's plan. 
+          If you attend an out-of-state college however, your parent's plan may not extend coverage.
+          In those cases, most of the time your university's health insurance plan offers the best 
+          competitve rates for health insurance. 
         </p>
       </Modal.Body>
       <Modal.Footer>
@@ -48,9 +134,9 @@ function App() {
 
   // filtering, recommends user which modal to click into
   if (hook.includes("insurance") || hook.includes("life")){
-    defaultMsg = "->Student Health Insurance";
-  } else if (hook.includes("earn")){
-    defaultMsg = "->Investment Calculator";
+    defaultMsg = "-> Student Health Insurance";
+  } else if (hook.includes("save")){
+    defaultMsg = "-> Savings Calculator";
   } else {
     defaultMsg = "What are your intentions?";
   }
@@ -64,7 +150,7 @@ function App() {
     </div>
     <div>
     <button variant="primary" id="investment" class="box1" onClick={() => setModalShow(true)}>
-      Investment Calculator
+      Savings Calculator
     </button>
     </div>
     <div>
@@ -78,8 +164,8 @@ function App() {
     </button>
     </div>
     <div>
-    <button variant="primary" id = "info" class="box4" onClick={() => setModalShow(true)}>
-      Student Insurance
+    <button variant="primary" id = "info" class="box4" onClick={() => setModal2Show(true)}>
+      Student Health Insurance
     </button>
     </div>
     <div>
@@ -88,9 +174,14 @@ function App() {
     </button>
     </div>
 
-    <MyVerticallyCenteredModal
+    <SavingsModal
       show={modalShow}
       onHide={() => setModalShow(false)}
+    />
+
+    <InfoModal 
+      show={modal2Show}
+      onHide={() => setModal2Show(false)}
     />
   </>
   );
